0ed5c5f0ac1837bac7f5189690fed3c7b81561a7
peyo <peyo@7332c9bf-690c-0410-b12f-844d5cfd0fe5>
2008-03-28 15:35:53 +0000

    Added options to prtree2paradist tests (notably Hilbert curve contiguous distribution).

--- branches/parallel/tests/prtree2paradist.c
+++ branches/parallel/tests/prtree2paradist.c
@@ -10,16 +10,19 @@
 #include <math.h>
 #include <glib.h>
 
-
+/* option variables */
+static gboolean _do_contiguous = TRUE;
 static gboolean _do_write = FALSE;
-
-static gint rk, sz;
-
-static GPtrArray *points = NULL;
-
 static gint _np = 12;
 static guint32 _random_seed = 0;
 static gint _flush_interval = 10;
+static gboolean _hilbert = FALSE;
+static gboolean _scatter_before = FALSE;
+static gboolean _put_regions = TRUE;
+static gboolean _verbose = FALSE;
+static gint rk, sz;
+
+static GPtrArray *points = NULL;
 
 static gpointer pt_alloc (gboolean resident, gpointer user_data)
 {
@@ -131,8 +134,11 @@ static gpointer rg_alloc (gboolean resident, gpointer user_data)
 static void rg_destroy (gpointer data, gboolean resident,
                         gpointer user_data)
 {
+  Circle *rg = data;
 /*   g_printerr ("%d: destroy 1 Circle (%p)\n", rk, data); */
-/*   contiguous_distribute_nodes (tree); */
+/*   g_printerr ("%d: destroy circle {c=", rk); */
+/*   vsg_vector2d_write (&rg->center, stderr); */
+/*   g_printerr (" r=%lf}\n", rg->radius); */
 
   g_ptr_array_remove (regions, data);
   g_free (data);
@@ -212,10 +218,9 @@ static void _pt_write (VsgVector2d *pt, FILE *file)
 
 static void _rg_write (Circle *c, FILE *file)
 {
-  fprintf (file, "<circle cx=\"%g\" cy=\"%g\" r=\"%g\" " \
-           "style=\"stroke:#000000;fill:none;\"/>\n",
-           c->center.x, -c->center.y, c->radius);
-
+  fprintf (file, "<circle cx=\"%g\" cy=\"%g\" r=\"%g\" "	\
+	   "style=\"stroke:#000000;fill:none;\"/>\n",
+	   c->center.x, -c->center.y, c->radius);
 }
 
 static void _traverse_bg_write (VsgPRTree2dNodeInfo *node_info, FILE *file)
@@ -243,7 +248,7 @@ static void _traverse_fg_write (VsgPRTree2dNodeInfo *node_info, FILE *file)
   g_slist_foreach (node_info->point_list, (GFunc) _pt_write, file);
 }
 
-static void _tree_write (VsgPRTree2d *tree)
+static void _tree_write (VsgPRTree2d *tree, gchar *prefix)
 {
   gchar fn[1024];
   FILE *f;
@@ -260,7 +265,7 @@ static void _tree_write (VsgPRTree2d *tree)
   w = ub.x - x;
   h = -lb.y - y;
 
-  g_sprintf (fn, "prtree2parallel-%03d.svg", rk);
+  g_sprintf (fn, "%s%03d.svg", prefix, rk);
   f = fopen (fn, "w");
 
   fprintf (f, "<?xml version=\"1.0\" standalone=\"no\"?>\n" \
@@ -403,6 +408,10 @@ static gint contiguous_dist (VsgPRTree2dNodeInfo *node_info,
 
       cda->current_lcount ++;
 
+/*       g_printerr ("%d: sending node ", rk); */
+/*       vsg_vector2d_write (&node_info->center, stderr); */
+/*       g_printerr (" to %d\n", ret); */
+
       return ret;
     }
 
@@ -433,6 +442,8 @@ static void contiguous_distribute_nodes (VsgPRTree2d *tree)
   MPI_Allreduce (array->data, reduced->data, array->len, MPI_INT, MPI_MAX,
                  MPI_COMM_WORLD);
 
+/*   g_printerr ("%d: contiguous allreduce size=%d\n", rk, array->len); */
+
   g_array_free (array, TRUE);
 
   cda.array = reduced;
@@ -454,6 +465,43 @@ static void contiguous_distribute_nodes (VsgPRTree2d *tree)
 
 }
 
+typedef enum _Hilbert2Key Hilbert2Key;
+enum _Hilbert2Key {
+  HK2_0_1,
+  HK2_3_2,
+  HK2_3_1,
+  HK2_0_2,
+
+};
+
+static gint hilbert2_coords[][4] = {
+  {0, 2, 3, 1, },
+  {3, 1, 0, 2, },
+  {3, 2, 0, 1, },
+  {0, 1, 3, 2, },
+
+};
+
+static Hilbert2Key hilbert2_decompositions[][4] = {
+  {HK2_0_2, HK2_0_1, HK2_0_1, HK2_3_1, },
+  {HK2_3_1, HK2_3_2, HK2_3_2, HK2_0_2, },
+  {HK2_3_2, HK2_3_1, HK2_3_1, HK2_0_1, },
+  {HK2_0_1, HK2_0_2, HK2_0_2, HK2_3_2, },
+
+};
+
+static void hilbert2_order (gpointer node_key, gint *children,
+                            gpointer *children_keys)
+{
+  gint i;
+  Hilbert2Key hkey = GPOINTER_TO_INT (node_key);
+
+  for (i=0; i<4; i++)
+    {
+      children[i] = hilbert2_coords[hkey][i];
+      children_keys[i] = GINT_TO_POINTER (hilbert2_decompositions[hkey][i]);
+    }
+}
 static void random_fill (VsgPRTree2d *tree, guint np);
 
 static void (*_fill) (VsgPRTree2d *tree, guint np) = random_fill;
@@ -482,17 +530,21 @@ static void random_fill (VsgPRTree2d *tree, guint np)
 
       if (i%sz != rk) continue;
 
+      if (i % 10000 == 0 && _verbose) g_printerr ("%d: insert %dth point\n", rk, i);
+
       pt = pt_alloc (TRUE, NULL);
       pt->x = x1;
       pt->y = y1;
       vsg_prtree2d_insert_point (tree, pt);
 
-      c = rg_alloc (TRUE, NULL);
-      c->center.x = x2;
-      c->center.y = y2;
-      c->radius = r;
-      vsg_prtree2d_insert_region (tree, c);
-
+      if (_put_regions)
+        {
+          c = rg_alloc (TRUE, NULL);
+          c->center.x = x2;
+          c->center.y = y2;
+          c->radius = r;
+          vsg_prtree2d_insert_region (tree, c);
+        }
 /*       g_printerr ("%d: %d\n", rk, i); */
     }
 
@@ -511,7 +563,7 @@ void parse_args (int argc, char **argv)
     {
       arg = argv[iarg];
 
-      if (g_ascii_strcasecmp (arg, "-np") == 0)
+      if (g_ascii_strncasecmp (arg, "--np", 4) == 0)
 	{
 	  guint tmp = 0;
 	  iarg ++;
@@ -523,9 +575,42 @@ void parse_args (int argc, char **argv)
 	  else
 	    g_printerr ("Invalid particles number (-np %s)\n", arg);
 	}
+      else if (g_ascii_strncasecmp (arg, "--seed", 6) == 0)
+	{
+	  guint tmp = 0;
+	  iarg ++;
+
+	  arg = (iarg<argc) ? argv[iarg] : NULL;
+
+	  if (sscanf (arg, "%u", &tmp) == 1)
+            _random_seed = tmp;
+	  else
+	    g_printerr ("Invalid random seed (--seed %s)\n", arg);
+	}
+      else if (g_strncasecmp (arg, "--no-regions", 12) == 0)
+        {
+          _put_regions = FALSE;
+        }
+      else if (g_strncasecmp (arg, "--no-dist", 9) == 0)
+        {
+          _do_contiguous = FALSE;
+        }
+      else if (g_strncasecmp (arg, "--hilbert", 9) == 0)
+        {
+          _hilbert = TRUE;
+        }
+      else if (g_strncasecmp (arg, "--scatter", 9) == 0)
+        {
+          _scatter_before = TRUE;
+        }
       else if (g_strncasecmp (arg, "--write", 7) == 0)
         {
           _do_write = TRUE;
+        }
+      else if (g_strncasecmp (arg, "-v", 2) == 0 ||
+               g_strncasecmp (arg, "--verbose", 9) == 0)
+        {
+          _verbose = TRUE;
         }
       else if (g_ascii_strcasecmp (arg, "--version") == 0)
 	{
@@ -541,11 +626,34 @@ void parse_args (int argc, char **argv)
     }
 }
 
+static void _rg_dump (Circle *c, FILE *f)
+{
+  fprintf (f, "%g %g %g\n", c->center.x, c->center.y, c->radius);
+}
+
+static void _traverse_rg_dump (VsgPRTree2dNodeInfo *node_info, FILE *file)
+{
+
+  if (rk == 0 || VSG_PRTREE2D_NODE_INFO_IS_LOCAL (node_info))
+    g_slist_foreach (node_info->region_list, (GFunc) _rg_dump, file);
+}
+
+static void _write_regions (VsgPRTree2d *tree, gchar *prefix)
+{
+  gchar fn[1024];
+  FILE *file;
+
+  sprintf (fn, "%s%03d.txt", prefix, rk);
+  file = fopen (fn, "w");
+
+  vsg_prtree2d_traverse (tree, G_PRE_ORDER,
+                         (VsgPRTree2dFunc) _traverse_rg_dump, file);
+
+}
 
 gint main (gint argc, gchar ** argv)
 {
   gint ret = 0;
-  gint i;
 
   VsgPRTree2d *tree;
 
@@ -557,6 +665,9 @@ gint main (gint argc, gchar ** argv)
 
   MPI_Comm_size (MPI_COMM_WORLD, &sz);
   MPI_Comm_rank (MPI_COMM_WORLD, &rk);
+
+/* #define BUFSIZE 1024*1024*64 */
+/*   MPI_Buffer_attach (malloc (BUFSIZE), BUFSIZE); */
 
   vsg_init_gdouble ();
 
@@ -575,6 +686,13 @@ gint main (gint argc, gchar ** argv)
                            (VsgPoint2dDistFunc) vsg_vector2d_dist,
                            (VsgRegion2dLocFunc) _circle_loc2, 2);
 
+  if (_hilbert)
+    {
+      /* configure for hilbert curve order traversal */
+      vsg_prtree2d_set_children_order (tree, hilbert2_order,
+                                       GINT_TO_POINTER (HK2_0_1));
+    }
+
 /*   MPI_Barrier (MPI_COMM_WORLD); */
 /*   g_printerr ("%d: set_parallel begin\n", rk); */
 
@@ -589,27 +707,60 @@ gint main (gint argc, gchar ** argv)
   init_total_points_count ();
   init_total_regions_count ();
 
-/*   MPI_Barrier (MPI_COMM_WORLD); */
-/*   g_printerr ("%d: distribute_nodes begin\n", rk); */
+  if (_scatter_before)
+    {
+      gint i = 0;
 
-/*   i = 0; */
-/*   vsg_prtree2d_distribute_nodes (tree, */
-/*                                  (VsgPRTree2dDistributionFunc) */
-/*                                  scatter_dist, */
-/*                                  &i); */
+      if (_verbose)
+        {
+          MPI_Barrier (MPI_COMM_WORLD);
+          g_printerr ("%d: scatter nodes begin\n", rk);
+        }
 
-  contiguous_distribute_nodes (tree);
+      vsg_prtree2d_distribute_nodes (tree,
+                                     (VsgPRTree2dDistributionFunc)
+                                     scatter_dist,
+                                     &i);
 
-  ret += check_points_number (tree);
-  ret += check_regions_number (tree);
+      ret += check_points_number (tree);
+      ret += check_regions_number (tree);
 
-/*   MPI_Barrier (MPI_COMM_WORLD); */
-/*   g_printerr ("%d: distribute_nodes ok\n", rk); */
+      if (_verbose)
+        {
+          MPI_Barrier (MPI_COMM_WORLD);
+          g_printerr ("%d: scatter nodes ok\n", rk);
+        }
+
+      _write_regions (tree, "rg-scatter");
+      _tree_write (tree, "scatter-");
+    }
+
+  if (_do_contiguous)
+    {
+      if (_verbose)
+        {
+          MPI_Barrier (MPI_COMM_WORLD);
+          g_printerr ("%d: contiguous distribute begin\n", rk);
+        }
+
+      contiguous_distribute_nodes (tree);
+
+      ret += check_points_number (tree);
+      ret += check_regions_number (tree);
+
+      if (_verbose)
+        {
+          MPI_Barrier (MPI_COMM_WORLD);
+          g_printerr ("%d: contiguous distribute ok\n", rk);
+        }
+
+      _write_regions (tree, "rg-contiguous");
+    }
 
   if (_do_write)
     {
       MPI_Barrier (MPI_COMM_WORLD);
-      _tree_write (tree);
+      _tree_write (tree, "prtree2parallel-");
     }
 
   if (_do_write)
