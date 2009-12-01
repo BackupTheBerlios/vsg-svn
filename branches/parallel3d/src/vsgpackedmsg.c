e585d3fab8f53b7c48654e43196cf136ddf755b4
peyo <peyo@7332c9bf-690c-0410-b12f-844d5cfd0fe5>
2009-12-01 14:00:41 +0000

    added VsgPackedMsg trace mechanism for debugging

--- branches/parallel3d/src/vsgpackedmsg.c
+++ branches/parallel3d/src/vsgpackedmsg.c
@@ -1,6 +1,286 @@
 #include "vsgpackedmsg.h"
 
-#include "string.h"
+#include <vsg-config.h>
+
+#include <string.h>
+#include <stdio.h>
+
+#include <glib/gprintf.h>
+#include <stdarg.h>
+
+#ifdef VSG_PACKED_MSG_TRACE
+
+typedef gint64 _trace_id;
+#define _MPI_TRACE_ID_TYPE MPI_LONG_LONG
+#define _PM_ID_SIZE sizeof (_trace_id)
+#define _PM_BEGIN_POS _PM_ID_SIZE
+
+FILE *_trace_file = NULL;
+static gboolean _trace_active = TRUE;
+static _trace_id _msgid = 0;
+static _trace_id _msgid_incr = 1;
+static GHashTable *_trace_comm_hash = NULL;
+/* static gint _trace_id_max = 0; */
+static GTimer *_timer = NULL;
+
+static void _trace_atexit ()
+{
+  if (_trace_file != NULL)
+    {
+      fclose (_trace_file);
+      _trace_file = NULL;
+
+      g_hash_table_destroy (_trace_comm_hash);
+      g_timer_destroy (_timer);
+    }
+}
+
+static MPI_Fint _trace_write_comm (MPI_Comm comm)
+{
+  MPI_Fint ret = MPI_Comm_c2f (comm);
+  MPI_Group world_group, group;
+  gint rk, sz, i;
+  gint world_rk, world_sz;
+  gint *world_rks, *comm_rks;
+
+  MPI_Comm_group (MPI_COMM_WORLD, &world_group);
+  MPI_Comm_group (comm, &group);
+
+  MPI_Comm_rank (MPI_COMM_WORLD, &world_rk);
+  MPI_Comm_size (MPI_COMM_WORLD, &world_sz);
+
+  MPI_Comm_rank (comm, &rk);
+  MPI_Comm_size (comm, &sz);
+
+  world_rks = g_malloc (world_sz * sizeof (int));
+  comm_rks = g_malloc (world_sz * sizeof (int));
+
+  for (i=0; i<world_sz; i++)
+    world_rks[i] = i;
+
+  MPI_Group_translate_ranks (world_group, world_sz, world_rks, group, comm_rks);
+
+  g_fprintf (_trace_file, "comm=%d rk=%d sz=%d translate={",
+	     ret, rk, sz);
+
+  for (i=0; i<world_sz; i++)
+    {
+      g_fprintf (_trace_file, "%d ", comm_rks[i]);
+    }
+
+  g_fprintf (_trace_file, "}\n");
+
+  MPI_Group_free (&world_group);
+  MPI_Group_free (&group);
+
+  g_free (world_rks);
+  g_free (comm_rks);
+
+  return ret;
+}
+
+static gint _trace_comm_id (MPI_Comm comm)
+{
+  gint *ret = g_hash_table_lookup (_trace_comm_hash, comm);
+
+  if (ret == NULL)
+    {
+      ret = g_malloc (sizeof (gint));
+
+      *ret = _trace_write_comm (comm);
+
+      g_hash_table_insert (_trace_comm_hash, comm, ret);
+    }
+
+  return *ret;
+}
+
+static inline void _trace_file_open ()
+{
+  if (_trace_file == NULL)
+    {
+      gint rk, sz;
+      gchar filename[256];
+      GTimeVal now;
+      const gchar *trace_dir = g_getenv ("VSG_PACKED_MSG_TRACE_DIR");
+      gchar *trace_path;
+
+      MPI_Comm_rank (MPI_COMM_WORLD, &rk);
+      MPI_Comm_size (MPI_COMM_WORLD, &sz);
+
+      _msgid = rk+1;
+      _msgid_incr = sz;
+
+      g_sprintf (filename, "vsg-packed-msg-%04d.trace", rk);
+
+      if (trace_dir != NULL)
+	trace_path = g_build_filename (trace_dir, filename, NULL);
+      else
+	trace_path = g_strdup (filename);
+
+      g_printerr ("%d : opening trace \"%s\"\n", rk, trace_path);
+
+      _trace_file = fopen (trace_path, "w");
+
+      g_free (trace_path);
+
+      _trace_comm_hash = g_hash_table_new_full (g_direct_hash, g_direct_equal,
+						NULL, g_free);
+
+      g_get_current_time (&now);
+      _timer = g_timer_new ();
+
+      g_fprintf (_trace_file, "opening processor %d: %ld %ld\n", rk, now.tv_sec, now.tv_usec);
+
+      _trace_comm_id (MPI_COMM_WORLD);
+      _trace_comm_id (MPI_COMM_SELF);
+
+      g_atexit (_trace_atexit);
+    }
+}
+
+void vsg_packed_msg_trace_set_active (gboolean active)
+{
+  _trace_active = active;
+}
+
+gboolean vsg_packed_msg_trace_get_active ()
+{
+  return _trace_active;
+}
+
+static void _trace_write_msg (gchar *name, MPI_Comm comm, _trace_id msgid,
+			      gint remote, gint tag, gint size)
+{
+  gdouble seconds;
+  gulong microseconds;
+  gint commid;
+
+  _trace_file_open ();
+
+  commid = _trace_comm_id (comm);
+
+  if (_trace_active)
+    {
+      seconds = g_timer_elapsed (_timer, NULL);
+      microseconds = (glong) (seconds * 1.e6);
+      g_fprintf (_trace_file, "%lu: msg=%s msgid=%ld comm=%d remote=%d tag=%d "
+		 "size=%d\n",
+		 microseconds, name,
+		 msgid, commid, remote, tag,
+		 (gint) (size - sizeof (_trace_id)));
+
+      fflush (_trace_file);
+    }
+}
+
+void vsg_packed_msg_trace (gchar *format, ...)
+{
+  gdouble seconds;
+  gulong microseconds;
+  va_list ap;
+
+  _trace_file_open ();
+
+  seconds = g_timer_elapsed (_timer, NULL);
+  microseconds = (glong) (seconds * 1.e6);
+
+  g_fprintf (_trace_file, "%lu: # ", microseconds);
+
+  va_start (ap, format);
+  g_vfprintf (_trace_file, format, ap);
+  va_end (ap);
+
+  g_fprintf (_trace_file, "\n");
+
+  fflush (_trace_file);
+}
+
+static inline void _trace_set_msgid (VsgPackedMsg *msg, _trace_id msgid)
+{
+  gint oldpos = msg->position;
+
+  msg->position = 0;
+
+  if (msg->buffer == NULL)
+    {
+      msg->buffer = g_malloc0 (_PM_ID_SIZE);
+      msg->size = _PM_ID_SIZE;
+    }
+
+  MPI_Pack (&msgid, 1, _MPI_TRACE_ID_TYPE, msg->buffer, msg->size,
+	    &msg->position, msg->communicator);
+
+  msg->position = MAX (msg->position, oldpos);
+
+  _msgid += _msgid_incr;
+}
+
+static void _trace_write_msg_send (VsgPackedMsg *msg, gchar *mode, gint dst,
+				   gint tag)
+{
+  _trace_id msgid;
+
+  _trace_file_open ();
+
+  msgid = _msgid;
+
+  if (msg->own_buffer)
+    {
+      /* set the msg id at the beginning of the message buffer */
+      _trace_set_msgid (msg, _msgid);
+    }
+  else
+    {
+      /* negative msgid for shared buffer messages */
+      msgid = - _msgid;
+
+      /* set the msg id at the beginning of the message buffer */
+      _trace_set_msgid (msg, msgid);
+    }
+
+  _trace_write_msg (mode, msg->communicator, msgid, dst, tag, msg->position);
+}
+
+static void _trace_write_msg_recv (VsgPackedMsg *msg, gchar *mode, gint src,
+				   gint tag)
+{
+  _trace_id msgid;
+
+  _trace_file_open ();
+
+  msg->position = 0;
+
+  vsg_packed_msg_recv_read (msg, &msgid, 1, _MPI_TRACE_ID_TYPE);
+
+  _trace_write_msg (mode, msg->communicator, msgid, src, tag, msg->size);
+}
+
+#else
+
+#define _PM_ID_SIZE (0)
+#define _PM_BEGIN_POS (0)
+
+#define _trace_file_open() /* noop */
+#define _trace_write_msg_send(msg, mode, dst, tag) /* noop */
+#define _trace_write_msg_recv(msg, mode, src, tag) /* noop */
+
+void vsg_packed_msg_trace (gchar *format, ...)
+{
+  /* noop */
+}
+
+void vsg_packed_msg_trace_set_active (gboolean active)
+{
+  /* noop */
+}
+
+gboolean vsg_packed_msg_trace_get_active ()
+{
+  return FALSE;
+}
+
+#endif
 
 /**
  * VSG_PACKED_MSG_STATIC_INIT:
@@ -49,10 +329,27 @@ void vsg_packed_msg_init (VsgPackedMsg *pm, MPI_Comm comm)
 
   pm->communicator = comm;
 
+#ifdef VSG_PACKED_MSG_TRACE
+  pm->buffer = g_malloc0 (sizeof (_trace_id));
+#else
   pm->buffer = NULL;
-  pm->position = 0;
-  pm->size = 0;
+#endif
+  pm->position = _PM_BEGIN_POS;
+  pm->size = _PM_ID_SIZE;
   pm->own_buffer = TRUE;
+}
+
+/**
+ * vsg_packed_msg_reset:
+ * @pm: a #VsgPackedMsg.
+ *
+ * 
+ */
+void vsg_packed_msg_reset (VsgPackedMsg *pm)
+{
+  g_return_if_fail (pm != NULL);
+
+  pm->position = _PM_BEGIN_POS;
 }
 
 /**
@@ -94,6 +391,16 @@ void vsg_packed_msg_send_append (VsgPackedMsg *pm, gpointer buf,
   g_return_if_fail (pm != NULL);
 
   g_assert (pm->own_buffer == TRUE);
+
+#ifdef VSG_PACKED_MSG_TRACE
+  /* init msg id */
+  if (pm->position == 0)
+    {
+      if (pm->buffer == NULL) pm->buffer = g_malloc0 (_PM_ID_SIZE);
+      pm->size = MAX (pm->size, _PM_ID_SIZE);
+      pm->position = _PM_BEGIN_POS;
+    }
+#endif
 
   pos = pm->position;
   size = pm->size;
@@ -163,6 +470,8 @@ void vsg_packed_msg_send (VsgPackedMsg *pm, gint dst, gint tag)
 {
   gint ierr;
 
+  _trace_write_msg_send (pm, "send", dst, tag);
+
   ierr = MPI_Send (pm->buffer, pm->position, MPI_PACKED, dst, tag,
                    pm->communicator);
 
@@ -183,6 +492,8 @@ void vsg_packed_msg_isend (VsgPackedMsg *pm, gint dst, gint tag,
                            MPI_Request *request)
 {
   gint ierr;
+
+  _trace_write_msg_send (pm, "isend", dst, tag);
 
   ierr = MPI_Isend (pm->buffer, pm->position, MPI_PACKED, dst, tag,
                     pm->communicator, request);
@@ -234,7 +545,9 @@ void vsg_packed_msg_recv (VsgPackedMsg *pm, gint src, gint tag)
   ierr = MPI_Recv (pm->buffer, rsize, MPI_PACKED, src, tag,
                    pm->communicator, &status);
 
-  pm->position = 0;
+  _trace_write_msg_recv (pm, "recv", src, tag);
+
+  pm->position = _PM_BEGIN_POS;
 
   if (ierr != MPI_SUCCESS) vsg_mpi_error_output (ierr);
 }
