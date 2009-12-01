bb2de9bbcb03b8b72b447f54fc3a5625f049f12c
peyo <peyo@7332c9bf-690c-0410-b12f-844d5cfd0fe5>
2009-12-01 20:01:44 +0000

    Close "parallel3d" branch (merged into branch "parallel")

--- branches/parallel3d/bindings/python/vsgtypedefs.py
+++
@@ -1,8 +0,0 @@
-import argtypes
-
-arg = argtypes.IntArg()
-argtypes.matcher.register('vsgloc2', arg)
-argtypes.matcher.register('vsgloc3', arg)
-argtypes.matcher.register('vsgrloc2', arg)
-argtypes.matcher.register('vsgrloc3', arg)
-
